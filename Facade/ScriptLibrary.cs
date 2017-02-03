cece14210aae5c05710879a6cb56328b8917d8d7
Giulio Vian <giuliovdev@gmail.com>
2017-02-03 22:33:35 +0100

    Reworked SendMail implementation dropping use of TeamFoundationMailService Class

--- Facade/ScriptLibrary.cs
+++ Facade/ScriptLibrary.cs
@@ -6,6 +6,8 @@ using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.Framework.Client;
 using Microsoft.TeamFoundation.Framework.Common;
 using Microsoft.TeamFoundation.Framework.Server;
+using System;
+using System.Net.Mail;
 
 namespace Aggregator.Core.Facade
 {
@@ -14,27 +16,83 @@ namespace Aggregator.Core.Facade
         private readonly ILogEvents logger;
         private readonly IRequestContext requestContext;
         private readonly ConnectionInfo connectionInfo;
+        private readonly Mailer mailer;
 
         public ScriptLibrary(IRuntimeContext context)
         {
             this.connectionInfo = context.GetConnectionInfo();
             this.requestContext = context.RequestContext;
             this.logger = context.Logger;
+            this.mailer = new Mailer(context.RequestContext.VssContext);
+        }
+
+        private class Mailer
+        {
+            private readonly bool Enabled;
+            private readonly string SmtpServer;
+            private readonly int SmtpPort;
+            private readonly bool EnableSsl;
+            private readonly MailAddress FromAddress;
+
+            internal Mailer(IVssRequestContext requestContext)
+            {
+                this.Enabled = false;
+                try
+                {
+                    TeamFoundationRegistryService service = requestContext.GetService<TeamFoundationRegistryService>();
+                    Microsoft.TeamFoundation.Framework.Server.RegistryEntryCollection registryEntryCollection = service.ReadEntriesFallThru(requestContext, FrameworkServerConstants.NotificationRootPath + "/*");
+                    if (registryEntryCollection["EmailEnabled"].GetValue<bool>(true))
+                    {
+                        this.SmtpServer = registryEntryCollection["SmtpServer"].GetValue(string.Empty);
+                        this.SmtpPort = registryEntryCollection["SmtpPort"].GetValue<int>(-1);
+                        this.EnableSsl = registryEntryCollection["SmtpEnableSsl"].GetValue<bool>(false);
+                        this.FromAddress = null;
+
+                        string value = registryEntryCollection["EmailNotificationFromAddress"].GetValue(string.Empty);
+                        if (!string.IsNullOrEmpty(value) && !string.IsNullOrEmpty(this.SmtpServer))
+                        {
+                            this.FromAddress = new MailAddress(value);
+                            this.Enabled = true;
+                        }
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine($"SendMail failed: {ex.Message}");
+                }
+            }
+
+            internal void Send(string to, string subject, string body)
+            {
+                if (this.Enabled)
+                {
+                    using (SmtpClient client = new SmtpClient())
+                    {
+                        client.Host = this.SmtpServer;
+                        client.Port = this.SmtpPort;
+                        client.EnableSsl = this.EnableSsl;
+
+                        MailMessage message = new MailMessage();
+                        message.From = this.FromAddress;
+                        message.To.Add(to);
+                        message.Subject = subject;
+                        message.Body = body;
+
+                        client.Send(message);
+                    }
+                }
+            }
         }
 
         public void SendMail(string to, string subject, string body)
         {
-            var mailService = new TeamFoundationMailService();
-            var vssContext = this.requestContext.VssContext;
-            mailService.LoadSettings(vssContext);
-
-            string from = mailService.FromAddress.Address;
-
-            this.logger.LibrarySendMail(from, to, subject, body);
-            using (var message = new System.Net.Mail.MailMessage(from, to, subject, body))
+            try
             {
-                mailService.ValidateMessage(vssContext, message);
-                mailService.Send(vssContext, message);
+                this.mailer.Send(to, subject, body);
+            }
+            catch (System.Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"SendMail failed: {ex.Message}");
             }
         }
 
